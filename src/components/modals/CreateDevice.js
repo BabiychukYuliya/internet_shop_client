247f4879f9b129808b377bc4b6db72ee9674dfc4
BabiychukYuliya <julia4233097@gmail.com>
2024-01-25 23:21:26 +0200

    Update CreateDevice.js

--- src/components/modals/CreateDevice.js
+++ src/components/modals/CreateDevice.js
@@ -1,6 +1,19 @@
-import { Modal, Button, Form } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Modal, Button, Form, Dropdown, Row, Col } from "react-bootstrap";
+import { Context } from "../../index";
 
 const CreateDevice = ({ show, onHide }) => {
+  const { device } = useContext(Context);
+  const [info, setInfo] = useState([]); //масив характеристик для пристрою
+
+  const addInfo = () => {
+    setInfo([...info, { title: "", description: "", number: Date.now() }]);
+  };
+
+  const removeInfo = (number) => {
+    setInfo(info.filter((i) => i.number !== number));
+  };
+
   return (
     <Modal
       show={show}
@@ -14,11 +27,62 @@ const CreateDevice = ({ show, onHide }) => {
           Додати пристрій
         </Modal.Title>
       </Modal.Header>
+
       <Modal.Body>
         <Form>
-          <Form.Control placeholder="Введіть назву пристрою" />
+          <Dropdown>
+            <Dropdown.Toggle>Оберіть тип</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.types.map((type) => (
+                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown>
+            <Dropdown.Toggle style={{ marginTop: 10 }}>
+              Оберіть бренд
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.brands.map((brand) => (
+                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Form.Control
+            placeholder="Введіть назву пристрою"
+            style={{ marginTop: 10 }}
+          />
+          <Form.Control
+            placeholder="Введіть вартість пристрою"
+            type="number"
+            style={{ marginTop: 10 }}
+          />
+          <Form.Control type="file" style={{ marginTop: 10 }} />
+          <hr />
+          <Button variant="outline-dark" onClick={addInfo}>
+            Додати нову властивість
+          </Button>
+          {info.map((i) => (
+            <Row key={i.number} style={{ marginTop: 10 }}>
+              <Col>
+                <Form.Control placeholder="Введіть назву властивості" />
+              </Col>
+              <Col>
+                <Form.Control placeholder="Введіть опис властивості" />
+              </Col>
+              <Col>
+                <Button
+                  variant="outline-danger"
+                  onClick={() => removeInfo(i.number)}
+                >
+                  Видалити властивість
+                </Button>
+              </Col>
+            </Row>
+          ))}
         </Form>
       </Modal.Body>
+
       <Modal.Footer>
         <Button variant="outline-success" onClick={onHide}>
           Додати пристрій
